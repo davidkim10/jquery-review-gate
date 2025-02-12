3aa61367396481da8427bcf0dbe3f1f23de78e92
David Kim <davidykim10@gmail.com>
2025-02-12 10:51:24 -0500

    add bundler to create dist files

---
+++ build.js
@@ -0,0 +1,34 @@
+const esbuild = require("esbuild");
+const copy = require("esbuild-plugin-copy").default;
+
+// Build for JavaScript
+esbuild
+  .build({
+    entryPoints: ["src/js/review-gate.jquery.js"],
+    bundle: true,
+    minify: true,
+    outfile: "dist/js/review-gate.jquery.min.js",
+    target: ["ie11"],
+    plugins: [
+      copy({
+        assets: [
+          {
+            from: "src/js/jquery.emojiRatings.min.js",
+            to: "./",
+            keepStructure: false,
+          },
+        ],
+      }),
+    ],
+  })
+  .catch(() => process.exit(1));
+
+// Build for CSS
+esbuild
+  .build({
+    entryPoints: ["src/css/styles.css"],
+    bundle: false,
+    minify: true,
+    outfile: "dist/css/review-gate.min.css",
+  })
+  .catch(() => process.exit(1));
--- src/js/review-gate.jquery.js
+++ src/js/review-gate.jquery.js
@@ -1,21 +1,23 @@
+import "./jquery.emojiRatings.min.js";
+
 (function ($) {
   $.fn.reviewGate = function (options) {
     var defaults = {
-      stepClassName: 'review-step',
-      navbarColor: '#fff',
-      navBarClassName: 'review-gate-navbar',
-      navBarBackBtnClassName: 'review-gate-navbar__btn',
+      stepClassName: "review-step",
+      navbarColor: "#fff",
+      navBarClassName: "review-gate-navbar",
+      navBarBackBtnClassName: "review-gate-navbar__btn",
       enableNavBackBtn: true,
       emojiConfig: {
-        emoji: 'star',
+        emoji: "star",
         fontSize: 42,
         attributes: {
-          id: 'emoji-ratings-wrapper',
+          id: "emoji-ratings-wrapper",
         },
         css: {},
       },
       onUpdate: function () {
-        alert('Add an onUpdate function');
+        alert("Add an onUpdate function");
       },
     };
 
@@ -26,35 +28,35 @@
 
     // Selectors
     var $selector = this;
-    var $navbar = $('.' + settings.navBarClassName);
-    var $navBarBackBtn = $('.' + settings.navBarBackBtnClassName);
+    var $navbar = $("." + settings.navBarClassName);
+    var $navBarBackBtn = $("." + settings.navBarBackBtnClassName);
 
     // -- Utility Functions
     var findCurrentStep = function () {
-      return $selector.find('.current').attr('data-step');
+      return $selector.find(".current").attr("data-step");
     };
 
     var updateCurrentStep = function (step) {
       var currentStep = step || findCurrentStep();
       window.reviewGateSettings.currentStep = currentStep;
-      $('body').attr('data-current-step', currentStep);
+      $("body").attr("data-current-step", currentStep);
     };
 
     var handleNavBackBtnDisplay = function () {
       var step = window.reviewGateSettings.currentStep;
       if (step > 1) {
-        $navBarBackBtn.removeClass('hide');
+        $navBarBackBtn.removeClass("hide");
       } else {
-        $navBarBackBtn.addClass('hide');
+        $navBarBackBtn.addClass("hide");
       }
     };
 
     var handleOnStepChange = function (step, callback) {
-      $('.review-step.current').fadeOut(function () {
-        $('.review-step.current').removeClass('current');
+      $(".review-step.current").fadeOut(function () {
+        $(".review-step.current").removeClass("current");
         var target = $($selector).find("[data-step='".concat(step, "']"));
         target.fadeIn(400);
-        target.addClass('current');
+        target.addClass("current");
         updateCurrentStep(step);
         callback();
       });
@@ -64,8 +66,8 @@
 
     var initNavBar = function () {
       $navbar.fadeIn();
-      $navbar.css('background-color', settings.navbarColor);
-      $navBarBackBtn.addClass('hide');
+      $navbar.css("background-color", settings.navbarColor);
+      $navBarBackBtn.addClass("hide");
 
       if (settings.enableNavBackBtn) {
         $navBarBackBtn.click(function () {
@@ -75,26 +77,26 @@
     };
 
     var initSteps = function () {
-      var $step = $('.' + settings.stepClassName);
-      !$step.hasClass('current') && $step.first().addClass('current');
+      var $step = $("." + settings.stepClassName);
+      !$step.hasClass("current") && $step.first().addClass("current");
       $step.each(function (index) {
-        $(this).attr('data-step', index + 1);
+        $(this).attr("data-step", index + 1);
       });
     };
 
     var initEmojiRating = function () {
-      var emojiWrapper = $('<div />')
+      var emojiWrapper = $("<div />")
         .attr(settings.emojiConfig.attributes)
         .css(settings.emojiConfig.css)
         .appendTo($selector.children()[0]);
 
       if (!$.fn.emojiRating) {
-        $.getScript('./js/jquery.emojiRatings.min.js', function () {
+        $.getScript("./js/jquery.emojiRatings.min.js", function () {
           try {
             $.fn.emojiRating;
             emojiWrapper.emojiRating(emojiConfig);
           } catch (err) {
-            throw new Error('emojiRating library is a required dependency');
+            throw new Error("emojiRating library is a required dependency");
           }
         });
       } else {
@@ -124,11 +126,11 @@
         this,
         Array.prototype.slice.call(arguments, 1)
       );
-    } else if (typeof options === 'object' || !options) {
+    } else if (typeof options === "object" || !options) {
       // Default to "init"
       return methods.init.apply(this, arguments);
     } else {
-      $.error('Method ' + options + ' does not exist');
+      $.error("Method " + options + " does not exist");
     }
 
     return this;
